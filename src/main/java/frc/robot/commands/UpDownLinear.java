22ac2519729232b94c03db34ff1af7f29dba2f02
Ziyao-Ma <ziyaoma12@gmail.com>
2024-09-11 14:41:09 -0400

    asdf

--- src/main/java/frc/robot/commands/UpDownLinear.java
+++ src/main/java/frc/robot/commands/UpDownLinear.java
@@ -1,18 +1,37 @@
 package frc.robot.commands;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class UpDownLinear extends Command{
+    int time = 0;
+    int seconds = 10;
+    boolean increasing = true;
+
     private TalonSRX motors;
     public UpDownLinear(TalonSRX motors){
+
         this.motors = motors;
     }
 
     @Override
-    public void execute(){
-        
+    public void execute(){ //0.02 seconds period
+        motors.set(TalonSRXControlMode.PercentOutput, time/(50));//after one second, called 50 times, want voltage to be 1/12
+        //time * percent * 12d/(50d * seconds
+        if(time == 50){
+            increasing = false;
+        }
+        if(time == 0){
+            increasing = true;
+        }
+        if(increasing){
+            time++;
+        }
+        else{
+            time--;
+        }
     }
     
 }
