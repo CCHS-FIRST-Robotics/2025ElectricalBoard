b85723981145365545bf85b384d83354c9f1854b
Robotics Laptop 2 <75389927+ColinBalfour@users.noreply.github.com>
2024-10-03 15:59:10 -0400

    uwu

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.*;
 import frc.robot.commands.MoveToAngle;
-//import frc.robot.commands.UpDownLinear;
+import frc.robot.commands.UpDownLinear;
 //import frc.robot.commands.UpDownQuadratic;
 
 public class RobotContainer {
@@ -27,9 +27,9 @@ public class RobotContainer {
     private void configureBindings() {
         System.out.println("why");
         // MoveToAngle temp = new MoveToAngle(test, () -> controller.getLeftX(), () -> controller.getLeftY());
-        controller.x().onTrue( new MoveToAngle(test, () -> controller.getLeftX(), () -> controller.getLeftY()));
-         nintendo1.onTrue(new InstantCommand(() -> System.out.println("switch 1")));
-      //  controller.x().onTrue(new UpDownLinear(test));
+     //   controller.x().onTrue( new MoveToAngle(test, () -> controller.getLeftX(), () -> controller.getLeftY()));
+    //     nintendo1.onTrue(new InstantCommand(() -> System.out.println("switch 1")));
+        controller.x().toggleOnTrue(new UpDownLinear(test));
       //  controller.y().onTrue(new UpDownQuadratic(test));
       
     }
