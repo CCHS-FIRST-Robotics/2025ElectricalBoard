1664763e9fa680845794db938bbe7cf0dd5c764d
BlindmanFlamingFish <lukexgettings@gmail.com>
2024-10-08 15:05:31 -0400

    So much logic that can't be tested until it's too late :(

--- src/main/java/frc/robot/commands/Simon.java
+++ src/main/java/frc/robot/commands/Simon.java
@@ -1,18 +1,40 @@
 package frc.robot.commands;
 
+import java.util.ArrayList;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.motors.FourMotors;
 
 public class Simon extends Command{
- 
+    
+    //Mechanical stuff (ew)
     public FourMotors motors;
     public CommandXboxController controller;
+
     public double x;
     public double y;
 
     public double magnitude;
 
+    public double direction;
+
+    //Simon stuff (yay)
+    public final double[] directions = {-Math.PI, -Math.PI * 0.5, 0, Math.PI * 0.5}; //Radians, baby!
+    //                               north east           south    west
+    public ArrayList<Double> sequence = new ArrayList<Double>();
+    public double selectedDirection = 0;
+
+    public enum GameState{SEQUENCE, THINK, ACTIVATED, LOSE};
+    public GameState state = GameState.SEQUENCE;
+    public double input;
+
+    public int sequenceIndex = 0; //How far in the sequence we are
+    public int timer = 50;
+
+    boolean done = false;
+
+    //Other stuff idk
     public Simon(FourMotors motors, CommandXboxController controller){
         this.motors = motors;
         this.controller = controller;
@@ -20,20 +42,76 @@ public class Simon extends Command{
     }
 
     @Override
+    public void initialize(){
+        sequence.add(directions[(int) Math.round(Math.random() * 3)]);
+    }
+
+    @Override
     public void execute() {
-        x = controller.getLeftX();
-        y = controller.getLeftY();
-        magnitude = Math.sqrt(((x * x) + (y * y))); //Finding Magnitude
+        switch (state) {
+            case THINK: { //When the player's joystick is resting. Detecting if they move it out of deadzone
+                
+                x = controller.getLeftX();
+                y = controller.getLeftY();
+                magnitude = Math.sqrt(((x * x) + (y * y))); //Finding Magnitude
 
-        if (Math.abs(magnitude) < 0.06){
-            magnitude = 0;
-        }
-        else{
-            magnitude *= 12;
-            magnitude *= Math.signum(y);
-        }
+                if (Math.abs(magnitude) >= 0.5){
+                    //DO THE SIMON Stuff
+                    input = Math.round( (Math.atan2(y, x) * 2) / Math.PI ) * Math.PI * 0.5;
 
-        motors.setAllMotorVoltage(magnitude);
+                    state = GameState.ACTIVATED;
+                }
+
+                break;
+            }
+            case ACTIVATED:{
+                if (input == sequence.get(sequenceIndex)) { //Checking if correct
+                    if (sequence.size() != sequenceIndex + 1) { //Making sure not at end of sequence
+                        sequenceIndex++;
+                        //Rotate the talonFX
+                    }
+                    else { //If end of sequence
+                        //Rotate the motor I guess so player knows it's resetting
+                        sequenceIndex = 0;
+                        timer = 100;
+                        sequence.add(directions[(int) Math.round(Math.random() * 3)]);
+                        state = GameState.SEQUENCE;
+                    }
+                }
+                else {
+                    state = GameState.LOSE;
+                }
+                break;
+            }
+            case SEQUENCE: {
+                if (timer <= 0) {
+                    // set talonFX to sequence.get(sequenceIndex);
+                    if (sequence.size() == sequenceIndex + 1){
+                        sequenceIndex = 0;
+                        state = GameState.THINK;
+                    }
+                    else {
+                        sequenceIndex++;
+                        timer = 100;
+                    }
+                }
+                else {
+                    timer--;
+                }
+                break;
+            }
+        
+            case LOSE: {
+                done = true;
+            }
+
+            default:
+                break;
+        }
+    }
+
+    public boolean isFinished(){
+        return done;
     }
     
 }
