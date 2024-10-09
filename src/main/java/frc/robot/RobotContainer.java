1d093aaf4f40f396e15335ab88012486f3743f70
Jonathan Li <jonathanli668899@gmail.com>
2024-10-08 22:44:48 -0400

    (-?-)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -8,47 +10,37 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
     private final CommandXboxController transmitter = new CommandXboxController(Constants.Controller_PORT);
-    private final TalonFX Motor= new TalonFX(0);
+    private final TalonFX motor= new TalonFX(0);
     private final PIDController pidController = new PIDController(0.5, 0.0, 0.1);
 
+    private final double targetAngle = 90;
+
     public RobotContainer() {
-        pidController.setSetpoint(90); //90 deg (pi/2 rad)
+        pidController.setSetpoint(targetAngle); //90 deg (pi/2 rad)
         pidController.setTolerance(5, 10); //position tolerance, velocity tolerance
         configureBindings();
     }
 
     private void configureBindings() {
         Trigger Button = transmitter.b();
-        Button.onTrue(new InstantCommand(() -> moveToAngle(90)));
-  
+        Button.onTrue(new InstantCommand(() -> moveToAngle(targetAngle)));
+      
     }
 
-    /*public void moveToAngle(double angle) {
+    public void moveToAngle(double angle) {
       pidController.setSetpoint(angle);
-      double output = pidController.calculate(Motor.getPosition().getValue(), 90);
-      Motor.set(output);
+      double currentPosition = motor.getPosition().getValue();
+      double output = pidController.calculate(currentPosition);
+
+      //constrain the output to -1.0 to 1.0 as the controller joystick ranges from -1 to 1 and 1 = max throttle signal -1 = reverse direction max throttle?
+      MathUtil.clamp(pidController.calculate(output, targetAngle), -1, 1);
+
+      motor.set(output);
 
       if (pidController.atSetpoint()) {
-        Motor.set(0);
+        motor.set(0);
       }
   }
-  */
-
-  public void moveToAngle(double angle) {
-    pidController.setSetpoint(angle);
-    double currentPosition = Motor.getPosition().getValue();
-    double output = pidController.calculate(currentPosition);
-  
-    output = Math.max(Math.min(output, 1.0), -1.0);
-
-    Motor.set(output);
-
-    if (pidController.atSetpoint()) {
-        Motor.set(0);
-    }
-}
-
-
 
 }
 
